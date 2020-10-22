d7f337611e3d1e9dc83bfd5f58e3fae2c25c63f4
hannankan <1278501748@qq.com>
2020-10-22 13:02:31 +0800

    添加maven application module

---
+++ application/src/main/java/fdu/daslab/sgd/App.java
@@ -0,0 +1,13 @@
+package fdu.daslab.sgd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
